e2e43d63ea06ebc875210a2f5b3a28186831d967
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-02-01 19:34:22 -0500

    added all neccessary tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE employee_db;
+
 DROP DATABASE IF EXISTS employee_db;
 
 CREATE DATABASE employee_db;
@@ -9,5 +11,20 @@ CREATE TABLE  department (
 id INT PRIMARY KEY,
 
 name VARCHAR(30) NOT NULL
-
 ); 
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR (30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employees (
+   id INT PRIMARY KEY,
+   first_name VARCHAR(30),
+   last_name VARCHAR(30),
+   role_id  INT,
+   manager_id INT
+);
+
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees (id, name, salary) 
+VALUES  (1, 'John Doe', 50000),
+        (2, 'Jane Smith', 60000),
+        (3, 'Bob Johnson', 55000);
